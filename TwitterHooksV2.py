b2d4574a4b784f1b53af7ec60a9cbb2a29f3230c
Narayana Murari <ngowris1@umbc.edu>
2022-11-06 18:49:22 -0500

    Adding Organic metrics and saving non geo tweets

--- TwitterHooksV2.py
+++ TwitterHooksV2.py
@@ -4,6 +4,7 @@ import calendar
 import time
 import os.path
 from Botometer import generate_random_bot_scores
+from Util import create_organic_metrics
 
 # export 'BEARER_TOKEN'='<your_bearer_token>' and replace with your BearerToken
 bearer_token = 'PutYourKeyHere'
@@ -49,27 +50,38 @@ def get_recent_tweets_counts(keyword,size_per_keyword):
     # recent_tweets_url = "https://api.twitter.com/2/tweets/search/recent"
     # params = {'query': 'Bitcoin -is:retweet', 'tweet.fields': 'context_annotations,author_id,conversation_id,geo'}
     final_data_set = []
+    final_data_set_without_geo = []
     count = 0
-    data,next_token = getTweetsBatch(keyword, None)
+    data, tweets_without_geo, next_token = getTweetsBatch(keyword, None)
     final_data_set.extend(data)
+    final_data_set_without_geo.extend(tweets_without_geo)
     count = count + len(data)
     while count < size_per_keyword and next_token:
-        data, next_token = getTweetsBatch(keyword, next_token)
+        data, tweets_without_geo, next_token = getTweetsBatch(keyword, next_token)
         final_data_set.extend(data)
+        final_data_set_without_geo.extend(tweets_without_geo)
         count = count + len(data)
 
 
     print(keyword + ' - Tweet Count' +str(len(final_data_set)))
     #List: [{'author_id': '1150334633285955584', 'geo': {'place_id': '2a93711775303f90'}, 'context_annotations': [{'domain': {'id': '46', 'name': 'Business Taxonomy', 'description': 'Categories within Brand Verticals that narrow down the scope of Brands'}, 'entity': {'id': '1557696848252391426', 'name': 'Financial Services Business', 'description': 'Brands, companies, advertisers and every non-person handle with the profit intent related to Banks, Credit cards, Insurance, Investments, Stocks '}}, {'domain': {'id': '46', 'name': 'Business Taxonomy', 'description': 'Categories within Brand Verticals that narrow down the scope of Brands'}, 'entity': {'id': '1557697333571112960', 'name': 'Technology Business', 'description': 'Brands, companies, advertisers and every non-person handle with the profit intent related to softwares, apps, communication equipments, hardwares'}}, {'domain': {'id': '69', 'name': 'News Vertical', 'description': 'News Categories like Entertainment or Technology'}, 'entity': {'id': '1046545033657081857', 'name': 'News', 'description': 'News'}}, {'domain': {'id': '69', 'name': 'News Vertical', 'description': 'News Categories like Entertainment or Technology'}, 'entity': {'id': '1585285924069335040', 'name': 'Business & finance news'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '781974596148793345', 'name': 'Business & finance'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '840160819388141570', 'name': 'Tech news', 'description': 'Tech News'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '840161214890106880', 'name': 'Business news', 'description': 'Business & Finance News'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '848920371311001600', 'name': 'Technology', 'description': 'Technology and computing'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '1046545033657081857', 'name': 'News', 'description': 'News'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '1585285924069335040', 'name': 'Business & finance news'}}, {'domain': {'id': '10', 'name': 'Person', 'description': 'Named people in the world like Nelson Mandela'}, 'entity': {'id': '808713037230157824', 'name': 'Elon Musk', 'description': 'Elon Musk'}}, {'domain': {'id': '65', 'name': 'Interests and Hobbies Vertical', 'description': 'Top level interests and hobbies groupings, like Food or Travel'}, 'entity': {'id': '781974596148793345', 'name': 'Business & finance'}}, {'domain': {'id': '66', 'name': 'Interests and Hobbies Category', 'description': 'A grouping of interests and hobbies entities, like Novelty Food or Destinations'}, 'entity': {'id': '857878777191211008', 'name': 'Leadership', 'description': 'Leadership'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '808713037230157824', 'name': 'Elon Musk', 'description': 'Elon Musk'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '1091420346660470784', 'name': 'Tech personalities', 'description': 'Tech Professionals'}}, {'domain': {'id': '131', 'name': 'Unified Twitter Taxonomy', 'description': 'A taxonomy of user interests. '}, 'entity': {'id': '1166406108623163392', 'name': 'Business personalities'}}], 'edit_history_tweet_ids': ['1586385784671596547'], 'text': 'Elon Musk about to make twitter PG-13 as shit', 'conversation_id': '1586385784671596547', 'id': '1586385784671596547'}]
-    return final_data_set
+    return final_data_set,tweets_without_geo
 
 def filterForLocation(data):
     filtered_tweets = []
+    tweets_without_geo = []
     for tweet in data:
         if 'geo' in tweet:
             filtered_tweets.append(tweet)
+        else:
+            tweets_without_geo.append(tweet)
 
-    return filtered_tweets
+    # filtered_tweets_without_geo = filter_data_set(tweets_without_geo)
+    # convo_tweets_without_geo,usernames = get_tweet_conversation(filtered_tweets_without_geo)
+    # bot_data = get_bot_data(usernames)
+    # final_bot_data.update(bot_data)
+    # merged_json = merge_all_jsons(conversation_data, tweet_data)
+    return filtered_tweets, tweets_without_geo
 
 def getTweetsBatch(keywords, next_token):
     recent_tweets_url = "https://api.twitter.com/2/tweets/search/recent"
@@ -89,12 +101,12 @@ def getTweetsBatch(keywords, next_token):
         meta = response.json()['meta']
 
         # Will return only tweets which have location tagged to them
-        data = filterForLocation(data)
+        data, tweets_without_geo = filterForLocation(data)
         print(keywords+" : Found "+str(len(data))+" with location")
         if meta['next_token'] is not None:
-            return data, meta['next_token']
+            return data, tweets_without_geo, meta['next_token']
         else:
-            return data, None
+            return data, tweets_without_geo, None
 
 def filter_data_set(final_data_set):
     filtered_data_set = []
@@ -105,7 +117,8 @@ def filter_data_set(final_data_set):
                  'text': data['text'],
                  'geo': data['geo']['place_id'],
                  'created_at': data['created_at'],
-                 'public_metrics': data['public_metrics']
+                 'public_metrics': data['public_metrics'],
+                 'organic_metrics': create_organic_metrics()
                  }
 
         domains = set()
@@ -175,22 +188,52 @@ def build_dataset_for_keywords(requirements):
 
     final_json = []
     final_bot_data = {}
+
+    final_json_without_geo = []
     for data in requirements:
-        tweet_data = get_recent_tweets_counts(data['keyword'], data['count'])
+        tweet_data,tweet_data_without_geo = get_recent_tweets_counts(data['keyword'], data['count'])
         tweet_data = filter_data_set(tweet_data)
         tweet_data = get_location_details(tweet_data)
-        conversation_data,usernames = get_tweet_conversation(tweet_data)
+        conversation_data, usernames = get_tweet_conversation(tweet_data)
         bot_data = get_bot_data(usernames)
         final_bot_data.update(bot_data)
-        merged_json = merge_all_jsons(conversation_data,tweet_data)
+        merged_json = merge_all_jsons(conversation_data, tweet_data)
         final_json.extend(merged_json)
 
+        tweet_data_without_geo = filter_data_set_without_geo(tweet_data_without_geo)
+        conversation_data_without_geo, usernames_without_geo = get_tweet_conversation(tweet_data_without_geo)
+        bot_scores_without_geo = get_bot_data(usernames_without_geo)
+        final_bot_data.update(bot_scores_without_geo)
+        merged_json_without_geo = merge_all_jsons(conversation_data_without_geo,tweet_data_without_geo)
+        final_json_without_geo.extend(merged_json_without_geo)
 
     update_json_file('tweet_data',final_json)
     update_json_file('bot_data',final_bot_data)
-    # dumpToPickleDB('tweet_data', final_json, 'merged')
-    # dumpToPickleDB('bot_data', final_bot_data, 'merged')
+    update_json_file('tweet_data_without_geo',final_json_without_geo)
 
+#Updating country to no location for tweets without geo
+def filter_data_set_without_geo(final_data_set):
+    filtered_data_set = []
+    for data in final_data_set:
+        tweet = {'id': data['id'],
+                 'author_id': data['author_id'],
+                 'conversation_id': data['conversation_id'],
+                 'text': data['text'],
+                 'country': 'No Location',
+                 'created_at': data['created_at'],
+                 'public_metrics': data['public_metrics'],
+                 'organic_metrics': create_organic_metrics()
+                 }
+
+        domains = set()
+        if 'context_annotations' in data:
+            context_annotations = data['context_annotations']
+            for annotation in context_annotations:
+                domains.add(annotation['domain']['name'])
+
+        tweet['domains'] = list(domains)
+        filtered_data_set.append(tweet)
+    return filtered_data_set
 
 def merge_all_jsons(conversation_data,tweet_data):
     merged_json = []
@@ -206,10 +249,10 @@ def merge_all_jsons(conversation_data,tweet_data):
 if __name__ == "__main__":
     # Below is a list of keywords and no of tweets to fetch per key word
     requirements = [
-        {'keyword': 'Elon Musk', 'count': 1},
-        {'keyword': 'WorldWarIII', 'count': 1},
-        {'keyword': 'Midterms', 'count': 1},
-        {'keyword': 'Bitcoin', 'count': 1}
+        {'keyword': 'Bonus', 'count': 1},
+        {'keyword': 'Free', 'count': 1}
+        # {'keyword': 'Midterms', 'count': 1},
+        # {'keyword': 'Bitcoin', 'count': 1}
     ]
     build_dataset_for_keywords(requirements)
 
---
+++ Util.py
@@ -0,0 +1,25 @@
+from math import log10
+import random
+
+def create_organic_metrics():
+    """
+        "organic_metrics": {
+         "impression_count": 3880,
+         "like_count": 8,
+         "reply_count": 0,
+         "retweet_count": 4
+         "url_link_clicks": 3
+         "user_profile_clicks": 2
+        }
+    """
+    data = {
+        'impression_count': log10(random.randint(0, 1000000) + 100),
+        'like_count': log10(random.randint(0, 100000) + 100),
+        'reply_count': log10(random.randint(0, 1000) + 100),
+        'retweet_count': log10(random.randint(0, 1000) + 100),
+        'url_link_clicks': log10(random.randint(0, 1000) + 100),
+        'user_profile_clicks': log10(random.randint(0, 1000) + 100)
+    }
+    return data
+
+
